1c4b53f317b2b8e01568c4ddc959414330c36f0a
Amlan Mandal <157319926+Amlan042@users.noreply.github.com>
2024-05-22 12:42:46 +0000

    comparisons

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1);    //true
+// console.log("02" > 1);  //true
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0); //loose equality...it returns false
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);   //these 3 statements return false
+
+// === this is strict equlity checker
+//it not only checks the value but data type also
+// console.log("2" == 2);  //true
+// console.log("2" === 2); //fasle
+
+
