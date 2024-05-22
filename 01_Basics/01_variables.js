1f51e68d46457abf8e07dad763835d8b48b8d002
Amlan Mandal <157319926+Amlan042@users.noreply.github.com>
2024-05-22 10:09:54 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 4647487
+let accountEmail = "amlan4368@example.com"
+var accountPassword = "4746*35"
+accountCity = "Kolkata"  //variables can be declared without using any keyword but this is not preferable
+let accoutState;   //java takes this as "undefiend"
+
+// accountId = 346847 //changing const is not allowed
+accountEmail = "Amlan468@hotmail.com"
+accountPassword = "48479%737&"
+accountCity = "Hyderabad"
+
+/*Prefer not to use var
+bacause of issues in block scope and functional scope */
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accoutState])  //table is used to print the output in a tabular format
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use scrict";  //treat all JS code as newer version of JS
+
+//alert(3+3)  //we are using nodejs, not browser
+
+let myName = "Amlan"
+let age = 18
+let isLoggedin = false
+let state;
+
+//number
+//bigInt
+//String => " "
+//boolean  => true,false
+//null => standalone value
+//undefined => value is not assigned
+//symbol => used for uniqueness
+
+//Object
+
+console.log(typeof "Amlan");
+console.log(typeof age);
+console.log(typeof isLoggedin);
+
+console.log(typeof null);  //Object
+console.log(typeof undefined); //undefined
+
+
